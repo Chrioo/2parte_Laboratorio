e0315511251c6543f463c176657532bb044a35d4
Chris <88003200+Chrioo@users.noreply.github.com>
2021-08-30 19:02:44 -0600

    h

---
+++ SegundaParte/SegundaParte/Negocio/ClsPersona.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SegundaParte.Negocio
+{
+    class ClsPersona
+
+        {Persona per = new Persona(0, 0);
+        public String calcularIMC()
+        {
+
+            double pro = per.Peso / Math.Pow(per.Altura, 2);
+
+            if (pro < 20)
+            {
+                return "Su peso es ideal";
+            }
+            if (pro > 25)
+            {
+                return "Tiene sobre peso";
+            }
+            return "";
+        }
+        public String esMayorDeEdad()
+        {
+            if (per.Edad >= 18)
+            {
+                return "Es Mayor de edad";
+            }
+            else
+                return "No es mayor de edad";
+
+        }
+}
+}
--- SegundaParte/SegundaParte/Program.cs
+++ SegundaParte/SegundaParte/Program.cs
@@ -10,6 +10,8 @@ namespace SegundaParte
     {
         static void Main(string[] args)
         {
-        }
+           
+            }
+
     }
 }
